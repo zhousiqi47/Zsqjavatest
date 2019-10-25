1054ce379618f2a766e49f6b39b96b58403e2143
zhousiqi47 <1792807037@qq.com>
2019-10-25 11:51:40 +0800

    Loading

--- src/zhousiqi2/TimeTest.java
+++ src/zhousiqi2/TimeTest.java
@@ -6,6 +6,7 @@ public class TimeTest {
 		System.out.println(t1.getMin());
 		Time t2=new Time();
 		System.out.println(t2);
+		//t2.setMin(60);
 		t2.setTime(23, 59, 59);
 		System.out.println(t2);
 		System.out.println(t1.nextSecond());
